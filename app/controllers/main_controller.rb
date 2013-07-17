bda587cb53ebb104444d3be60bd267d1052998b7
Danish Mughal <mughal.danish@gmail.com>
2013-07-16 19:13:13 -0700

    added event model validations

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 	def home
-		@events = Event.where("start_time > ?", Time.now)
-		@pastevents = Event.where("end_time < ?", Time.now)
+		@events = Event.where("start_time >= ?", Time.now)
+		@pastevents = Event.where("start_time <= ?", Time.now)
 	end
 
 	def attend_event
--- app/models/event.rb
+++ app/models/event.rb
@@ -17,7 +17,30 @@
 
 class Event < ActiveRecord::Base
   attr_accessible :address, :category, :description, :end_time, :name, :start_time, :user_id, :venue
-
+  
+  # Basic relations to the other models
   belongs_to :user
   has_many :attendees
+
+  # Basic presence validations for the form. I've made the address optional but venue required.
+  validates :name, presence: true
+  validates :venue, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :user_id, presence: true
+
+  # Makes sure that the time inputs are valid. Function is below.
+  validate :time_scope
+
+	def time_scope
+		if start_time < Time.now
+			errors.add(:start_time, " is invalid or is for a past event.")
+		end
+		if end_time < start_time
+			errors.add(:end_time, " is invalid. Please check to make sure it's correct.")
+		end
+	end
+
 end
